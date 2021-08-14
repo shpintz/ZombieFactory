5ad8cc45a552ce665ba39bcfb2e583a279050222
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-08-14 17:26:53 -0400

    using etheriums hash function to genereate a 256-bit hex and returns a 16 bit deceimal

--- contract/ZombieFactory.sol
+++ contract/ZombieFactory.sol
@@ -22,7 +22,8 @@ contract ZombieFactory{
 
   // Private genereateRandomDna
   function _generateRandomDna(string memory _str) private view returns (uint) {
-
+    uint rand = uint(keccak256(abi.encodePacked(_str)));
+    return rand % dnaModulus;
   }
 
 }
