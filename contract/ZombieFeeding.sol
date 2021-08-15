14b7ae1f8f66da2ea013fed7733dff15fc4030ab
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-08-14 22:25:27 -0400

    making sure the zombie belongs to certain address and we grab zombie from the storage

--- contract/ZombieFeeding.sol
+++ contract/ZombieFeeding.sol
@@ -3,5 +3,8 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
-
+  // makes sure before we run function to check if zombie belongs to address
+  require(msg.sender == zombieToOwner[_zombieId]);
+  // We grab the zombie Dna and set it to myZombie
+  Zombie storage myZombie = zombies[_zombieId];
 }
