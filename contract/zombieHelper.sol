fccd66931b796ae68e87e395caa02b08cffb8e72
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-08-17 21:28:27 -0400

    adding 2 functions that check the level of zombies and changes name and or dna depnding on what level

--- contract/zombieHelper.sol
+++ contract/zombieHelper.sol
@@ -10,4 +10,25 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
+  // uses modifier to check make sure zombie is higher than level 2 to change name
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+    // make sure that person who called the function match the id of zombie
+    require( msg.sender == zombieToOwner[_zombieId]);
+    // grabs matching zombie id from zombie arry and changes the name
+    zombies[_zombieId].name = _newName;
+  }
+
+  // uses modifier to check make sure zombie is higher than level 2 to change name
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+    // make sure that person who called the function match the id of zombie
+    require(msg.sender == zombieToOwner[_zombieId]);
+    // grabs matching zombie id from zombie arry and changes the name
+    zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
+  }
+
 }
