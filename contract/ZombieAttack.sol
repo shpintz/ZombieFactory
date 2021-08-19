99dfc649cba0dad0176a542834c7d8eb14648ed6
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-08-18 23:45:11 -0400

    adding attack function w/varibale

--- contract/ZombieAttack.sol
+++ contract/ZombieAttack.sol
@@ -5,11 +5,17 @@ import "./zombiehelper.sol";
 contract ZombieAttack is ZombieHelper {
 
   uint randNonce = 0;
+  uint attackVictoryProbability = 70;
 
   // Funtion that generates random numbers with keccak (no secure)
    function randMod(uint _modulus) internal returns (uint){
     randNonce++;
     return uint(keccak256(abi.encodePacked(now , msg.sender, randNonce))) % _modulus;
+  }
+
+  // attack function
+  function attack( uint _zombieId, uint _targetId ) external {
+
   }
 
 }
