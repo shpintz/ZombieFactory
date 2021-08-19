09469ecb33b1e7506da28cceda107f766eec4fe3
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-08-18 23:42:10 -0400

    none secure way of adding functions

--- contract/ZombieAttack.sol
+++ contract/ZombieAttack.sol
@@ -4,4 +4,12 @@ import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
 
+  uint randNonce = 0;
+
+  // Funtion that generates random numbers with keccak (no secure)
+   function randMod(uint _modulus) internal returns (uint){
+    randNonce++;
+    return uint(keccak256(abi.encodePacked(now , msg.sender, randNonce))) % _modulus;
+  }
+
 }
